45f3befcda8fbb1034ad039ef09267149a4badb3
Pablo-2662 <pablo.munsas@sanviatorvalladolid.com>
2023-12-05 09:49:36 +0100

    :)

--- app/src/main/java/com/example/materialdesignb/MainActivity.java
+++ app/src/main/java/com/example/materialdesignb/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.materialdesignb;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -21,10 +23,12 @@ import android.widget.PopupMenu;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity implements NavHost {
+public class MainActivity extends AppCompatActivity implements NavHost, NavigationView.OnNavigationItemSelectedListener{
     private NavController navController;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +38,19 @@ public class MainActivity extends AppCompatActivity implements NavHost {
         //Esta parte declara el Top App Bar
         MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
         setSupportActionBar(topAppBar);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         //Aquí es dónde se debe insertar el Navigation Drawer, ya que es el icono izquierdo de la Top App Bar
         topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "AHHH", Snackbar.LENGTH_SHORT).show();
+                NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+                // Establece a esta actividad como el oyente para los elementos de navegación
+                navigationView.setNavigationItemSelectedListener(MainActivity.this);
+                ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, topAppBar, R.string.open, R.string.close);
+                drawerLayout.addDrawerListener(toggle);
+                toggle.syncState();
+
             }
         });
 
@@ -122,5 +133,23 @@ public class MainActivity extends AppCompatActivity implements NavHost {
     @Override
     public NavController getNavController() {
         return navController;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        boolean realizado = false;
+        Fragment fragmentoSeleccionado = null;
+        if (item.getItemId() == R.id.itemHome) {
+            fragmentoSeleccionado = new HomeFragment();
+            realizado = true;
+        } else if (item.getItemId() == R.id.itemTextInputs) {
+            fragmentoSeleccionado = new TextInputFragment();
+            realizado = true;
+        } else if (item.getItemId() == R.id.itemSearch) {
+            fragmentoSeleccionado = new SearchFragment();
+            realizado = true;
+        }
+        mostrarFragmento(fragmentoSeleccionado);
+        return realizado;
     }
 }
